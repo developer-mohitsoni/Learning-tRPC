0053e4b3bb1ec5ea40e060b62036ba52898e2768
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-20 18:16:50 +0530

    Refactor createContext function in [trpc].ts for creating Context

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,13 @@
 import { appRouter } from "@/server/routers/_app";
 import * as trpcNext from "@trpc/server/adapters/next";
 
+export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
+  return {
+    req: opts.req,
+    res: opts.res,
+  };
+};
+
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
