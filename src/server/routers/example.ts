fa6c8115265e95b5e7242b932777234596c41f11
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-20 17:39:59 +0530

    Add example router with getName procedure for getting full name

---
+++ src/server/routers/example.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { procedure, router } from "../trpc";
+
+export const exampleRouter = router({
+  getName: procedure
+    .input(z.object({ firstName: z.string(), lastName: z.string() }))
+    .query((opts) => {
+      return {
+        fullName: `${opts.input.firstName} ${opts.input.lastName}`,
+      };
+    }),
+});
+
+export type ExampleRouter = typeof exampleRouter;
