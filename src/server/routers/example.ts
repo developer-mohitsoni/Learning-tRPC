1e2b5df1222ec4d4c0fcffe4ccca5c7421bdb8b6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-20 18:23:31 +0530

    Refactor exampleRouter to remove console.log statement

--- src/server/routers/example.ts
+++ src/server/routers/example.ts
@@ -5,6 +5,8 @@ export const exampleRouter = router({
   getName: procedure
     .input(z.object({ firstName: z.string(), lastName: z.string() }))
     .query((opts) => {
+    //   console.log(opts.ctx.req);
+
       return {
         fullName: `${opts.input.firstName} ${opts.input.lastName}`,
       };
