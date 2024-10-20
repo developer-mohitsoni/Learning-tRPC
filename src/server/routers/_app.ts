11cb405c00219849756eeff568a317fdaecb6476
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-20 17:39:45 +0530

    Refactor _app.tsx to comment out unused getName procedure

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 import { procedure, router } from "../trpc";
 
 export const appRouter = router({
-  getName: procedure
-    .input(z.object({ firstName: z.string(), lastName: z.string() }))
-    .query((opts) => {
-      return {
-        fullName: `${opts.input.firstName} ${opts.input.lastName}`,
-      };
-    }),
+  // getName: procedure
+  //   .input(z.object({ firstName: z.string(), lastName: z.string() }))
+  //   .query((opts) => {
+  //     return {
+  //       fullName: `${opts.input.firstName} ${opts.input.lastName}`,
+  //     };
+  //   }),
 });
 
 export type AppRouter = typeof appRouter;
